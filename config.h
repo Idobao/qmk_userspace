7f2574d2abc47753b3fd5d0f5e63882f0eb8ca5c
Vino Rodrigues <github@m.vinorodrigues.com>
2022-09-03 16:29:37 +1000

    VIA_PROTOCOL_VERSION = 9

---
+++ config.h
@@ -0,0 +1,6 @@
+// Copyright 2022 IDOBAO (@idobao)
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#pragma once
+
+// do nothing ;)
---
+++ rules.mk
@@ -0,0 +1,6 @@
+# Copyright 2022 IDOBAO (@idobao)
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+OPT_DEFS += -DFLASH_BOOTLOADER   # enable KC_MAKE
+
+SRC += idobao.c
