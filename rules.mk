6ebb04090144be71885332bde88ea148b77b757f
Vino Rodrigues <366673+vinorodrigues@users.noreply.github.com>
2022-10-29 13:07:26 +1100

    Update rules.mk

--- rules.mk
+++ rules.mk
@@ -1,6 +1,6 @@
 # Copyright 2022 IDOBAO (@idobao)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-OPT_DEFS += -DFLASH_BOOTLOADER   # enable KC_MAKE
+OPT_DEFS += -DIDOBAO_USERSPACE  # enable KC_MAKE
 
 SRC += idobao.c
